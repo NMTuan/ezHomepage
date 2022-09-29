8f73955ec864bd28424447736cb1ca7321b8feac
NMTuan <nmtuan@qq.com>
2022-09-29 11:19:45 +0800

    feat: 尝试输出 tags 及 count

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,16 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-07-29 16:05:46
- * @LastEditTime: 2022-07-29 16:08:38
+ * @LastEditTime: 2022-09-28 17:54:47
  * @LastEditors: NMTuan
  * @Description: 
- * @FilePath: \ezBookmarks_web\layouts\default.vue
+ * @FilePath: \ezHomepage\layouts\default.vue
 -->
 <template>
     <div>
         <NuxtLink to="/">[index]</NuxtLink>
         <NuxtLink to="/page">[page]</NuxtLink>
+        <NuxtLink to="/tags">[tags]</NuxtLink>
         <slot />
     </div>
 </template>
---
+++ pages/tags.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author: NMTuan
+ * @Email: NMTuan@qq.com
+ * @Date: 2022-09-28 17:53:47
+ * @LastEditTime: 2022-09-29 10:40:44
+ * @LastEditors: NMTuan
+ * @Description: 
+ * @FilePath: \ezHomepage\pages\tags.vue
+-->
+<template>
+    <div>
+        <h1>tags</h1>
+        <pre>{{data}}</pre>
+    </div>
+</template>
+<script setup lang="ts">
+const directus = useDirectus()
+const data = ref([])
+
+directus.items('bookmarks_tags').readByQuery({
+    groupBy: [
+        'tags_name',
+    ],
+    aggregate: {
+        count: '*'
+    }
+})
+    .then(res => {
+        data.value = res.data
+    })
+</script>
